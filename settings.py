9f3aecc055e5844ed8131bd254fc800fd3c99798
Konjo <konjo007@gmail.com>
2011-05-05 12:13:18 +0200

    DZIALA CSS!! nie mozliwe?

--- settings.py
+++ settings.py
@@ -45,18 +45,18 @@ USE_L10N = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
-MEDIA_ROOT = ''
+MEDIA_ROOT = r'C:/Users/Konrad/Desktop/SQLArena/media/'
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
-MEDIA_URL = ''
+MEDIA_URL = '/media/'
 
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = '/'
+STATIC_ROOT = 'C:/Users/Konrad/Desktop/SQLArena/static/'
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
@@ -69,6 +69,7 @@ ADMIN_MEDIA_PREFIX = '/static/admin/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
+	'C:/Users/Konrad/Desktop/SQLArena/static',
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
@@ -103,7 +104,7 @@ MIDDLEWARE_CLASSES = (
 ROOT_URLCONF = 'SQLArena.urls'
 
 TEMPLATE_DIRS = (
-	'/home/alice/Projekty/Django/SQLArena/templates/',
+	'C:/Users/Konrad/Desktop/SQLArena/templates',
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
--- urls.py
+++ urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls.defaults import patterns, include, url
 import os.path
 from django.views.generic.simple import direct_to_template
+from django.views.static import *
+from django.conf import settings
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
@@ -17,6 +19,8 @@ urlpatterns = patterns('',
 #        'template': 'list.html'
 #    })
     # Uncomment the next line to enable the admin:
-    (r'', 'sprawdzarka.views.index'),
-    (r'^admin/', include(admin.site.urls)),
+    url(r'^$', 'sprawdzarka.views.index'),
+    url(r'^admin/', include(admin.site.urls)),
+	url(r'^media/(?P<path>.*)$', 'django.views.static.serve',
+	{'document_root': settings.MEDIA_ROOT}),
 )
